0995d544ff71ea5d489f781454ed485f7222b1dd
OHASHI Hideya <ohachige@gmail.com>
2016-03-06 22:41:03 +0900

    auto_connect: add patterns

--- auto_connect/main.go
+++ auto_connect/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"github.com/xthexder/go-jack"
 	"strings"
@@ -8,17 +9,61 @@ import (
 )
 
 var Client *jack.Client
-var Pair []string
+var Queue *list.List
 
-func list(tport string) {
-	tport_prefix := strings.Split(tport, ":")[0]
-	search := "jack_mixer:" + tport_prefix
+func connect_to(tport string) (port string, rev bool) {
+	port = ""
+	rev = false
+	x := strings.Split(tport, ":")
+	switch x[0] {
+	case "jack_mixer":
+		switch x[1] {
+		case "MAIN L":
+			port = "system:playback_1"
+		case "MAIN R":
+			port = "system:playback_2"
+		case "capture L":
+			port = "system:capture_1"
+			rev = true
+		case "capture R":
+			port = "system:capture_2"
+			rev = true
+		}
+	case "nekobee DSSI plugin":
+		port = "jack_mixer:nekobee"
+	case "ffmpeg":
+		switch x[1] {
+		case "input_1":
+			port = "jack_mixer:MAIN L"
+			rev = true
+		case "input_2":
+			port = "jack_mixer:MAIN R"
+			rev = true
+		}
+	case "alsa_in":
+		switch x[1] {
+		case "capture_1":
+			port = "jack_mixer:alsa L"
+		case "capture_2":
+			port = "jack_mixer:alsa R"
+		}
+	default:
+		port = "jack_mixer:" + x[0]
+	}
+	return
+}
+
+func portlist(tport string) {
+	connect_to, rev := connect_to(tport)
 	ports := Client.GetPorts("", "", 0)
 	for _, port := range ports {
-		if port == search {
+		if port == connect_to {
 			fmt.Println("match", tport, port)
-			Pair[0] = tport
-			Pair[1] = port
+			if rev {
+				Queue.PushBack([]string{port, tport})
+			} else {
+				Queue.PushBack([]string{tport, port})
+			}
 		}
 	}
 }
@@ -26,7 +71,7 @@ func list(tport string) {
 func update(id jack.PortId, st bool) {
 	if st {
 		port := Client.GetPortById(id)
-		list(port.String())
+		portlist(port.String())
 	}
 }
 
@@ -37,18 +82,20 @@ func main() {
 	}
 	defer client.Close()
 	Client = client
-	list("")
+	portlist("")
 	client.SetPortRegistrationCallback(update)
 	if code := client.Activate(); code != 0 {
 		panic("activate error")
 	}
-	Pair = make([]string, 2)
+	Queue = list.New()
 	for {
-		time.Sleep(time.Second * 1)
-		if Pair[0] != "" {
-			fmt.Println("get", Pair[0], Pair[1])
-			Client.Connect(Pair[0], Pair[1])
-			Pair[0] = ""
+		time.Sleep(time.Millisecond * 250)
+		v := Queue.Front()
+		if v != nil {
+			Queue.Remove(v)
+			x, _ := v.Value.([]string)
+			fmt.Println("get", x)
+			Client.Connect(x[0], x[1])
 		}
 	}
 }
