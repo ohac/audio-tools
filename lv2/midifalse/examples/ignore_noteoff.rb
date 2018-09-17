83a855931b3dae1b630ac8bd97cdd7d1a2e1a99f
OHASHI Hideya <ohachige@gmail.com>
2018-09-17 13:56:09 +0900

    retab

--- lv2/midifalse/examples/ignore_noteoff.rb
+++ lv2/midifalse/examples/ignore_noteoff.rb
@@ -3,8 +3,8 @@ require "./common.rb"
 
 program = [
   # initial stack: status note velocity
-	# status:
-	#   0x80 <= status < 0xf0: voice message
+  # status:
+  #   0x80 <= status < 0xf0: voice message
   #     0x8n: note off
   #     0x9n: note on
   #     0xan: pressure (after touch)
@@ -12,17 +12,17 @@ program = [
   #     0xcn: program change
   #     0xdn: pressure (after touch)
   #     0xen: pitch wheel change
-	#   0xf0, 0xf1, 0xf2, 0xf3, 0xf6: system message
-	#   0xf8, 0xfa, 0xfb, 0xfc, 0xfe, 0xff: system realtime message
-	#   < 0x80, 0xf4, 0xf5, 0xf7, 0xf9, 0xfd: invalid
+  #   0xf0, 0xf1, 0xf2, 0xf3, 0xf6: system message
+  #   0xf8, 0xfa, 0xfb, 0xfc, 0xfe, 0xff: system realtime message
+  #   < 0x80, 0xf4, 0xf5, 0xf7, 0xf9, 0xfd: invalid
 
-	S_OP_3DUP, S_OP_DROP, S_OP_DROP, # duplicate status to top
-	S_OP_16, S_OP_DIV, # top >>= 4
-	S_OP_8, S_OP_SUB, # top -= 8 
-	S_OP_3, S_OP_JZ, # if note off then jump to else
-	  S_OP_DROP, S_OP_RETURN,
-	# else
-	  S_OP_DROP, S_OP_DROP, S_OP_DROP, S_OP_DROP, S_OP_RETURN,
+  S_OP_3DUP, S_OP_DROP, S_OP_DROP, # duplicate status to top
+  S_OP_16, S_OP_DIV, # top >>= 4
+  S_OP_8, S_OP_SUB, # top -= 8
+  S_OP_3, S_OP_JZ, # if note off then jump to else
+    S_OP_DROP, S_OP_RETURN,
+  # else
+    S_OP_DROP, S_OP_DROP, S_OP_DROP, S_OP_DROP, S_OP_RETURN,
 ]
 
 result = program.join('')
--- lv2/midifalse/examples/vel_offset.rb
+++ lv2/midifalse/examples/vel_offset.rb
@@ -2,16 +2,16 @@
 require "./common.rb"
 
 program = [
-	S_OP_DUP, # duplicate velocity
+  S_OP_DUP, # duplicate velocity
   S_OP_16, S_OP_8, S_OP_MUL, S_OP_7, S_OP_SUB, # 128 - 7
-	S_OP_LESSTHAN, S_OP_5, S_OP_JZ, # if vel >= 121 jump to else
-	  S_OP_DROP,
-		S_OP_7, S_OP_ADD, # vel += 7
-	  S_OP_RETURN,
-	# else
-	  S_OP_DROP, S_OP_DROP,
-		S_OP_16, S_OP_8, S_OP_MUL, S_OP_1, S_OP_SUB, # 128 - 1
-		S_OP_RETURN,
+  S_OP_LESSTHAN, S_OP_5, S_OP_JZ, # if vel >= 121 jump to else
+    S_OP_DROP,
+    S_OP_7, S_OP_ADD, # vel += 7
+    S_OP_RETURN,
+  # else
+    S_OP_DROP, S_OP_DROP,
+    S_OP_16, S_OP_8, S_OP_MUL, S_OP_1, S_OP_SUB, # 128 - 1
+    S_OP_RETURN,
 ]
 
 result = program.join('')
